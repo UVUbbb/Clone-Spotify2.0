4355bfaaeb2bad7cf85c29b54ee46152c904b3a0
UVUbbb <nolan7193@gmail.com>
2021-12-03 18:03:12 +0800

    init commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className="">
+      <h1> This is a Dope Spotify 2.0 build! </h1>
+      
+    <main>
+      {/* Sidebar */}
+      {/* Center */}
+    </main>
+    
+    <div>{/*Player*/}</div>
+
+    </div>
+  )
+}
